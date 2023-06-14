c6b97c0cefe00d865a0dead5d59651ca224e6d31
Momin Riyadh <mominriyadh@gmail.com>
2023-06-14 16:56:09 +0600

    fix and deploy hamburger menu

---
+++ development/starter-code/assets/js/main.js
@@ -0,0 +1,32 @@
+window.onload = function () {
+    const openIcon = document.querySelector('.open');
+    const closeIcon = document.querySelector('.close');
+    const topNavArea = document.querySelector('.g-top-navigation');
+    document.addEventListener('click', function (e) {
+        if (e.target.matches('.g-hamburger')) {
+            openIcon.style.opacity = "0";
+            openIcon.style.display = "none";
+            closeIcon.style.opacity = "1";
+            closeIcon.style.display = "block";
+            topNavArea.classList.toggle('show');
+        } else if (e.target.matches('.close')) {
+            openIcon.style.opacity = "1";
+            openIcon.style.display = "block";
+            closeIcon.style.opacity = "0";
+            closeIcon.style.display = "none";
+            topNavArea.classList.remove('show');
+        } else if (e.target.matches('.open')) {
+            openIcon.style.opacity = "0";
+            openIcon.style.display = "none";
+            closeIcon.style.opacity = "1";
+            closeIcon.style.display = "block";
+            topNavArea.classList.toggle('show');
+        } else if (!e.target.closest('.g-top-navigation')) {
+            topNavArea.classList.remove('show');
+            openIcon.style.opacity = "1";
+            openIcon.style.display = "block";
+            closeIcon.style.opacity = "0";
+            closeIcon.style.display = "none";
+        }
+    });
+}
