2d2fcf1b53aedbf9f24c82872b3ae9016c3c8730
Momin Riyadh <mominriyadh@gmail.com>
2023-07-06 17:03:21 +0600

    fix and deploy 2.6

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,11 @@
-const http = require('')
+const http = require('http');
+
+function rqListener(req, res) {
+    console.log(req.url, req.method, req.headers);
+    res.setHeader('Content-Type', 'text/html');
+}
+
+const server = http.createServer(rqListener);
+server.listen(3000)
+
+
