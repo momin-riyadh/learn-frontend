a43a6413cfad6b0c5ac9b6808cc6b3fa894b8a82
momin-riyadh <mominriyadh@gmail.com>
2023-07-14 09:20:01 +0600

    fix and deploy

--- assets/main.js
+++ assets/main.js
@@ -3,6 +3,7 @@ const http = require('http');
 function rqListener(req, res) {
     console.log(req.url, req.method, req.headers);
     res.setHeader('Content-Type', 'text/html');
+    res.write("HEllo");
 }
 
 const server = http.createServer(rqListener);
