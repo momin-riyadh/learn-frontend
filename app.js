5ffbdc2b071350179f3cd9d7324bd8c63a8a3724
momin-riyadh <mominriyadh@gmail.com>
2023-07-14 09:34:12 +0600

    fix and deploy

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const app = express();
 
 app.use((req, res, next) => {
     console.log('In the Middleware');
+    next();
+});
+app.use((req, res, next) => {
+    console.log('In another Middleware');
+    //..
 });
 
 
