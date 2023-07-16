72498d710b9bc5f3970a1d4f5cf98372cb702d1b
Momin Riyadh <mominriyadh@gmail.com>
2023-07-16 16:58:21 +0600

    fix and deploy 2.8

---
+++ development/starter-code/assets/js/main.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function () {
+    gsap.fromTo(".banner-text", {
+            opacity: 0,
+            duration: 5,
+
+        },
+        {
+            opacity: 1,
+            duration: 5
+        });
+
+    gsap.fromTo(".banner-about-text, .plan-banner-text", {
+        opacity: 0,
+        duration: 5,
+
+    }, {opacity: 1, duration: 5})
+})
